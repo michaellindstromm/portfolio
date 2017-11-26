231c4e25c17db24636d8ab18fbe4f8fadf133280
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-26 14:33:52 -0600

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: "https://secure-harbor-91949.herokuapp.com",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',

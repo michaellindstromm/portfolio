6c6595e37ddb78b86cf4110966a2d1adfad4d116
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-26 14:39:47 -0600

    email test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,14 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Portfolio_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'https://secure-harbor-91949.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'secure-harbor-91949.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: "https://secure-harbor-91949.herokuapp.com",
+    domain: "secure-harbor-91949.herokuapp.com",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',

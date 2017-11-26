f8da6ad09f2f5adde91f3faf2abb0b1a01aa886c
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-26 14:04:54 -0600

    setting up heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Portfolio_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'secure-harbor-91949.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors= true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true  
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

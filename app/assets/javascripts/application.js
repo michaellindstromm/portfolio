65174bc31655e6ff635d906870c1975db1845a66
Michael Lindstrom <michaellindstr@gmail.com>
2017-11-27 23:00:39 -0600

    just need to figure out the projects section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,23 +18,38 @@
 
 $(window).scroll(function() {
 
-    console.log($(this).scrollTop());
+    var navbarHeight = $('#navbar').height();
 
-    if ($(this).scrollTop() > 735 && $(this).scrollTop() < 1490) {
+    if ($(this).scrollTop() > $('.about-section').offset().top - navbarHeight * 2 && $(this).scrollTop() < $('.jumbotron2').offset().top) {
         $('.navbar').removeClass('transparent-nav');
         $('.navbar').addClass('colored-nav');
-    } else if ($(this).scrollTop() > 2050) {
+    } else if ($(this).scrollTop() > $('.project-section').offset().top - navbarHeight * 1.5) {
         $('.navbar').removeClass('transparent-nav');
         $('.navbar').addClass('colored-nav');
     } else {
         $('.navbar').removeClass('colored-nav');
         $('.navbar').addClass('transparent-nav');
     }
+    
+    if ($(this).scrollTop() > 1250) {
+
+        $('#jumbo2-saying').removeClass('hideme');
+        $('#jumbo2-horizontal').removeClass('hideme');
+        $('#jumbo2-quoter').removeClass('hideme');
+    
+        $('#jumbo2-saying').addClass('jumbo-phrase');
+        $('#jumbo2-horizontal').addClass('jumbo-phrase');
+        $('#jumbo2-quoter').addClass('jumbo-phrase');
+        
+    }
 
 });
 
 function load() {
     $(window).on('load', function() {
+
+        $('body').addClass('fade-in');
+
         setTimeout(function() {
             $('#jumbo-name').removeClass('hideme');
             $('#jumbo-name').addClass('jumbo-name');
@@ -42,9 +57,14 @@ function load() {
         }, 1000);
         
         setTimeout(function() {
+            $('#jumbo-horizontal').removeClass('hideme');
+            $('#jumbo-horizontal').addClass('jumbo-phrase');
+        }, 2500);
+
+        setTimeout(function() {
             $('#jumbo-phrase').removeClass('hideme');
             $('#jumbo-phrase').addClass('jumbo-phrase');
-        }, 3000);
+        }, 4000);
     });
 }
 
